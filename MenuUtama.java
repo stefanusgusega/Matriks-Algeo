2a7fbb49201032df01089c30112a49c67966193f
stefanusgusega <gunawanstefanus006@gmail.com>
2019-09-20 22:27:23 +0700

    nambahin menu utama

--- MenuUtama.java
+++ MenuUtama.java
@@ -1,8 +1,83 @@
+import java.util.Scanner;
 class MenuUtama {
     public static void main (String[] args) {
         System.out.println("MENU");
         System.out.println("1. Sistem Persamaan Linier");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks balikan");
+        System.out.println("4. Matriks kofaktor");
+        System.out.println("5. Adjoin");
+        System.out.println("6. Interpolasi Polinom");
+        System.out.println("7. Keluar");
+        Scanner keyboard = new Scanner(System.in);
+        int i = keyboard.nextInt();
         clearScreen();
+        switch (i) {
+            case 1:
+            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("2. Metode eliminasi Gauss-Jordan");
+            System.out.println("3. Metode matriks balikan");
+            System.out.println("4. Kaidah Cramer");
+            int j = keyboard.nextInt();
+            clearScreen();
+            switch(j) {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+            break;
+            case 2:
+            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("2. Metode eliminasi Gauss-Jordan");
+            System.out.println("3. Metode matriks balikan");
+            System.out.println("4. Kaidah Cramer");
+            int k = keyboard.nextInt();
+            clearScreen();
+            switch(k) {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+            break;
+            case 3:
+            System.out.println("1. Metode eliminasi Gauss");
+            System.out.println("2. Metode eliminasi Gauss-Jordan");
+            System.out.println("3. Metode matriks balikan");
+            System.out.println("4. Kaidah Cramer");
+            int l = keyboard.nextInt();
+            clearScreen();
+            switch(l) {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+            case 7:
+            // keluar program //
+            break;
+        }
+        keyboard.close();
     }
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");  
