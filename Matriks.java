61a8294340dcaeeef9eef23ffef471de6eb507e2
stefanusgusega <gunawanstefanus006@gmail.com>
2019-09-20 22:38:00 +0700

    nambahin matriks punya fano

--- Matriks.java
+++ Matriks.java
@@ -1,7 +1,271 @@
-class MATRIKS {
-    int Mem[][];
-    int NBrsEff;
-    int NKolEff;
-}
+import java.util.Scanner;
 
-public class 
+public class matriks
+{
+        Scanner sc = new Scanner(System.in); 
+        
+        public int NBrsEff;
+        public int NKolEff;
+        float[][]mem= new float[50][50];
+
+        public static final int barmax = 50;
+        public static final int kolmax = 50;
+
+        void MakeMATRIKS (int NB, int NK)
+        /* Membentuk sebuah MATRIKS "kosong" yang siap diisi berukuran NB x NK di "ujung kiri" memori */
+        /* I.S. NB dan NK adalah valid untuk memori matriks yang dibuat */
+        /* F.S. Matriks M sesuai dengan definisi di atas terbentuk */
+        {
+        this.NBrsEff= NB;
+        this.NKolEff = NK;
+        }
+
+
+        boolean IsIdxValid (int i, int j)
+        /* Mengirimkan true jika i, j adalah indeks yang valid untuk matriks apa pun */
+        {
+        return ((i>=1 && i<=barmax)&&(j>=1 && j<=kolmax));
+        }
+
+       
+        boolean IsIdxEff (int i, int j)
+        /* Mengirimkan true jika i, j adalah indeks efektif bagi M */
+        {
+        return ((i>=1)&&(i<=NBrsEff)&&(j>=1)&&(j<=NKolEff));
+        }
+      
+
+        void BacaMATRIKS (int NB, int NK)
+        /* I.S. IsIdxValid(NB,NK) */
+        /* F.S. M terdefinisi nilai elemen efektifnya, berukuran NB x NK */
+        /* Proses: Melakukan MakeMATRIKS(M,NB,NK) dan mengisi nilai efektifnya */
+        /* Selanjutnya membaca nilai elemen per baris dan kolom */
+        /* Contoh: Jika NB = 3 dan NK = 3, maka contoh cara membaca isi matriks :
+        1 2 3
+        4 5 6
+        8 9 10
+        */
+        {
+        MakeMATRIKS(NB,NK);
+        int i,j;
+        for (i=1;i<=NBrsEff;i++)
+        {
+                for (j=1;j<=NKolEff;j++)
+                {
+                mem[i][j]=sc.nextInt();
+                }
+        }
+        
+        }
+        void TulisMATRIKS() {
+                MakeMATRIKS(NBrsEff,NKolEff);
+		System.out.println("\nMatriksnya adalah :");
+		for(int i=1; i <=NBrsEff; i++) {
+			for(int j=1; j <=NKolEff; j++) {
+				System.out.print("\t" + mem[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+        boolean IsFound1Kolom (int acbar,int ackol)
+        /* I.S Kolom Valid*/
+        /* F.S Menghasilkan true jika ditemukannya angka 1 di kol acuan */
+        {
+                int i;
+                for (i=acbar;i<=NBrsEff;i++)
+                {
+                        if (mem[i][ackol]==1)
+                        {
+                                return true;
+                        }
+                }
+                return false;
+        }
+
+        int IdxKolom1 (int acbar,int ackol)
+        /* I.S Kolom Valid*/
+        /* F.S Menghasilkan indeks baris pertama kali ditemukannya angka 1 di kol acuan, jika tidak ketemu kembaliaannya 0 */
+        {
+                int i;
+                for (i=acbar;i<=NBrsEff;i++)
+                {
+                        if (mem[i][ackol]==1)
+                        {
+                                return i;
+                        }
+                }
+                return 0;
+        }
+
+        boolean IsFound0Kolom (int acbar,int ackol)
+        /* I.S Kolom Valid*/
+        /* F.S Menghasilkan true jika  ditemukannya angka 0 di kol acuan */
+        {
+                int i;
+                for (i=acbar;i<=NBrsEff;i++)
+                {
+                        if (mem[i][ackol]==0)
+                        {
+                                return true;
+                        }
+                }
+                return false;
+        }
+
+        int IdxKolom0 (int acbar,int ackol)
+        /* I.S Kolom Valid*/
+        /* F.S Menghasilkan indeks baris pertama kali ditemukannya angka 0 di kol acuan, jika tidak ketemu kembaliaannya 0 */
+        {
+                int i;
+                for (i=acbar;i<=NBrsEff;i++)
+                {
+                        if (mem[i][ackol]==0)
+                        {
+                                return i;
+                        }
+                }
+                return 0;
+        }
+
+        void OBEtukar (int bartukar, int acbar)
+        {
+                int j;
+                for (j=1;j<=NKolEff;j++)
+                {
+                        float temp;
+                        temp=mem[(acbar)][(j)];
+                        mem[(acbar)][(j)]=mem[(bartukar)][(j)];
+                        mem[(bartukar)][(j)]=temp;
+                }
+        }
+
+        void OBEkurang (int barkurang, int kolkurang, int acbar, int ackol)
+        {
+                int j;
+                float ac;
+                ac = mem[(barkurang)][(kolkurang)]/mem[(acbar)][(ackol)];
+                if (mem[(barkurang)][(kolkurang)]!=0)
+                {
+                        for (j=kolkurang;j<=NKolEff;j++)
+                        {
+                        mem[(barkurang)][(j)]=mem[(barkurang)][(j)]-mem[(acbar)][(j)]*ac; 
+                        }
+                }
+        }
+
+        void OBEUtama (int barkurang, int ackol)
+        {
+                int j,ac1,ac2;
+                ac1=barkurang;
+                ac2=ackol;
+                for (j=1;j<=NKolEff;j++)
+                {
+                       mem[barkurang][j]=mem[barkurang][j]/mem[ac1][ac2];
+                }
+        }
+
+        void GaussSPL ()
+        /* Berlaku matriks lebih dari 2*2 */
+        {
+                int i,j,k,w,utama;
+                boolean keluar;
+                k=0;
+                keluar=false;
+                for (j=1;j<=NKolEff;j++)
+                {
+                        if (IsIdxEff(k+1,j))
+                        {
+                                for (i=1+k;i<=NBrsEff;i++)
+                                {
+                                        utama=1+k;
+                                        if (i==utama)
+                                        //saat baris ke i mula2
+                                        {
+                                                keluar=false;
+                                                w=0;
+                                                if (IsFound1Kolom(i+k,j))
+                                                //Kalau dalam baris ke i terdapat 1 maka ditukar dengan baris ke 1+k
+                                                {
+                                                        OBEtukar(IdxKolom1(i+k,j),j);
+                                                }  
+                                                
+                                                else
+                                                //saat tidak ada 1
+                                                {
+                                                        if (mem[i+k][j]==0 )
+                                                        //kalau di baris ke 1+k terdapat 0, maka ditukar sampai bukan 0
+                                                        //kalau ternyata 0 semua, maka next langsung ke step berikutnya(tidk dilakukan aksi appun)
+                                                        {
+                                                                while ((mem[i+k][j]==0 )&& (!(keluar)))
+                                                                {
+                                                                        w=w+1;
+                                                                        if ((i+k+w)!=NBrsEff)
+                                                                        {
+                                                                                keluar=true;
+                                                                                
+                                                                        }
+                                                                        else
+                                                                        {
+                                                                                OBEtukar(i+k+w,i+k);
+                                                                                if(mem[i+k][j]==0)
+                                                                                {
+                                                                                        OBEtukar(i+k+w,i+k);
+                                                                                }
+                                                                                else
+                                                                                {
+                                                                                        OBEUtama(i+k,j);
+                                                                                }
+                                                                        }
+                                                                }
+                                                        }
+                                                        else
+                                                        {
+                                                                OBEUtama (i+k,j);
+                                                        }
+                                                      
+                                                 }
+
+                                        }
+                                        else
+                                        //saat tidak mencari 1 utama
+                                        {
+
+                                                if ((mem[i+k][j]!=0 ))
+                                                {
+                                                        OBEkurang(i+k,j,utama,j);
+                                                }
+                                                
+
+                                        }
+                                }
+                        }
+
+                        if (keluar==false)
+                        {
+                         k=k+1;
+                         System.out.println("www");
+                        }
+                        
+                }
+
+        }
+
+        public static void main (String[] args)
+        {
+                int brs,kol;
+                Scanner sc = new Scanner(System.in); 
+                System.out.println("Baris :");
+                brs = sc.nextInt();
+                System.out.println("Kolom :");
+                kol = sc.nextInt();
+
+                matriks M = new matriks ();
+                M.BacaMATRIKS(brs,kol);
+                M.TulisMATRIKS();
+                M.GaussSPL();
+                M.TulisMATRIKS();
+
+
+        }
+    }
